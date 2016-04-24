ce93e6e7dc18f1c3365a452aa97d04e87f445e02
ViksataK <viktor.klisurov@gmail.com>
2016-04-24 17:11:28 +0300

    coding skills

--- coding_skills.py
+++ coding_skills.py
@@ -1,1 +1,20 @@
+import json
 
+
+def read_json():
+    dic = {'C++': ['', 0], 'PHP': ['', 0], 'Python': ['', 0], 'C#': ['', 0], 'Haskell': ['', 0], 'Java': ['', 0], 'JavaScript': ['', 0], 'Ruby': ['', 0],'CSS': ['', 0],'C': ['', 0]}
+    with open('data.json', 'r') as f:
+        data = json.load(f)
+        for dude in data['people']:
+            ime = dude['first_name']
+            lastime = dude['last_name']
+            for item in dude['skills']:
+                if item['level'] > dic[item['name']][1]:
+                    dic[item['name']][0] = ime + " " + lastime
+                    dic[item['name']][1] = item['level']
+
+    return dic
+
+
+
+print(read_json())
